c80dd03ea53f09ee9ba6ec1d156513baa621e6f3
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-06 23:32:20 +0100

    chore: init app

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import rssFile from "./assets/cssreflex.xml?raw";
+  import ArticleCard from "./components/ArticleCard.svelte";
+
+  const data = new DOMParser().parseFromString(rssFile, "text/xml");
+  const items = data.querySelectorAll("item");
+  console.log(items);
+  
+
+</script>
+
+<main>
+  <h1>readii</h1>
+  <p>A simple, open-source, and privacy-focused RSS reader.</p>
+  {#each [...items] as data}
+    <ArticleCard item={data} />
+  {/each}
+</main>
+
+<style>
+</style>
---
+++ src/components/ArticleCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { formatDistanceToNow, format } from "date-fns";
+
+    let { item }: { item: Element } = $props();
+    const title = item.querySelector("title")?.innerHTML;
+
+    const publishedAtStr = item.getElementsByTagName("pubDate")[0].innerHTML;
+    const publishedAt = new Date(publishedAtStr);
+    const dateToNow = formatDistanceToNow(publishedAt);
+    const htmlDatetime = format(publishedAt, "yyyy-MM-dd");
+
+    const creator = item.getElementsByTagName("dc:creator")[0].innerHTML.replace("<![CDATA[", "").replace("]]>", "");
+
+    const urlStr = item.querySelector("link")?.innerHTML;
+    const url = urlStr ? new URL(urlStr) : null;
+    const articleUrl = url ? url.href : null;
+    const baseUrl = url ? url.origin : null;
+</script>
+
+<article>
+  <h1>{title}</h1>
+  <time datetime={htmlDatetime}>{dateToNow}</time>
+  <a href={baseUrl} rel="noopener noreferrer">{creator}</a>
+  <!-- {@html item.getElementsByTagName("content:encoded")[0].innerHTML.replace("<![CDATA[", "").replace("]]>", "")} -->
+  <a href={articleUrl} target="_blank" rel="noopener noreferrer">
+    Visit
+  </a>
+</article>
