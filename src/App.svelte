4940a35b77aceb7e595bd39fc33f9d4684669ecd
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-09 13:26:32 +0100

    chore: add to App, add ArticleCard

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,67 @@
   const data = new DOMParser().parseFromString(rssFile, "text/xml");
   const items = data.querySelectorAll("item");
   console.log(items);
-  
 
+  class OPMLParser {
+    url?: string;
+    rawFile?: string;
+
+    constructor(input: string) {
+      try {
+        const url = new URL(input);
+        this.url = url.toString();
+      } catch {
+        this.rawFile = input;
+      }
+    }
+
+    async parse() {
+      if (!this.rawFile) {
+        if (this.url) {
+          const response = await fetch(this.url, {
+            mode: "cors",
+            headers: {
+              "Content-Type": "text/xml",
+            },
+          });
+          const text = await response.text();
+          this.rawFile = text;
+          console.log("response", {
+            response,
+            text,
+          });
+        } else {
+          throw new Error(
+            "Expected either a `url` or a `rawFile` to be defined. Make sure to pass it when creating an `OPMLParser` instance."
+          );
+        }
+      }
+      
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(this.rawFile, "text/xml");
+      const outlines = doc.querySelectorAll("outline");
+      const feeds = Array.from(outlines).map((outline) => {
+        return {
+          title: outline.getAttribute("title"),
+          xmlUrl: outline.getAttribute("xmlUrl"),
+          htmlUrl: outline.getAttribute("htmlUrl"),
+        };
+      });
+      return feeds;
+    }
+  }
+
+  const opml = new OPMLParser("https://nerdy.dev/subscriptions.opml");
+  console.log(opml.parse());
 </script>
 
-<main>
+<header>
   <h1>readii</h1>
-  <p>A simple, open-source, and privacy-focused RSS reader.</p>
+  <div>
+    <button>Import</button>
+  </div>
+</header>
+<main>
   {#each [...items] as data}
     <ArticleCard item={data} />
   {/each}
--- src/components/ArticleCard.svelte
+++ src/components/ArticleCard.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
   import { formatDistanceToNow, format } from "date-fns";
 
-    let { item }: { item: Element } = $props();
-    const title = item.querySelector("title")?.innerHTML;
+  let { item }: { item: Element } = $props();
+  const title = item.querySelector("title")?.innerHTML;
 
-    const publishedAtStr = item.getElementsByTagName("pubDate")[0].innerHTML;
-    const publishedAt = new Date(publishedAtStr);
-    const dateToNow = formatDistanceToNow(publishedAt);
-    const htmlDatetime = format(publishedAt, "yyyy-MM-dd");
+  const publishedAtStr = item.getElementsByTagName("pubDate")[0].innerHTML;
+  const publishedAt = new Date(publishedAtStr);
+  const dateToNow = formatDistanceToNow(publishedAt);
+  const htmlDatetime = format(publishedAt, "yyyy-MM-dd");
 
-    const creator = item.getElementsByTagName("dc:creator")[0].innerHTML.replace("<![CDATA[", "").replace("]]>", "");
+  const creator = item
+    .getElementsByTagName("dc:creator")[0]
+    .innerHTML.replace("<![CDATA[", "")
+    .replace("]]>", "");
 
-    const urlStr = item.querySelector("link")?.innerHTML;
-    const url = urlStr ? new URL(urlStr) : null;
-    const articleUrl = url ? url.href : null;
-    const baseUrl = url ? url.origin : null;
+  const urlStr = item.querySelector("link")?.innerHTML;
+  const url = urlStr ? new URL(urlStr) : null;
+  const articleUrl = url ? url.href : null;
+  const baseUrl = url ? url.origin : null;
 </script>
 
 <article>
   <h1>{title}</h1>
   <time datetime={htmlDatetime}>{dateToNow}</time>
-  <a href={baseUrl} rel="noopener noreferrer">{creator}</a>
+  <address>
+    By <a href={baseUrl} rel="author noopener noreferrer">{creator}</a>
+  </address>
   <!-- {@html item.getElementsByTagName("content:encoded")[0].innerHTML.replace("<![CDATA[", "").replace("]]>", "")} -->
-  <a href={articleUrl} target="_blank" rel="noopener noreferrer">
-    Visit
-  </a>
+  <a href={articleUrl} target="_blank" rel="noopener noreferrer"> Visit </a>
 </article>
