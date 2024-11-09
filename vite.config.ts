41b8696687632c13e00c67dce229a9d5377d687e
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-09 13:26:18 +0100

    chore: add lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import browserslist from 'browserslist';
+import {browserslistToTargets} from 'lightningcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+  ],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%'))
+    }
+  },
+});
