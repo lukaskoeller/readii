f39799c9010d678ff0d8f62fa2d94e36bf618cde
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-28 16:09:44 +0100

    chore: add shadow to AISummarizer

--- src/components/AISummarizer.svelte
+++ src/components/AISummarizer.svelte
@@ -99,5 +99,6 @@
     padding: var(--spacing-near);
     border-radius: var(--border-radius-medium);
     background-color: var(--color-brand-secondary-base);
+    box-shadow: 0px 2px 0px 0px var(--color-brand-secondary-emphasis);
   }
 </style>
