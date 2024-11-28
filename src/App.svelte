a97472db27c67a16344ad6b486da7420b6fc5ab2
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-28 15:34:06 +0100

    chore: update App markup

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,37 @@
 <script lang="ts">
-  import rssFile from "./assets/leaverou.xml?raw";
-  import rssFile2 from "./assets/cssreflex.xml?raw";
-  import rssFile3 from "./assets/echojs.xml?raw";
   import ArticleCard from "./components/ArticleCard.svelte";
   import Header from "./components/Header.svelte";
-  import { RSSNode, RSSParser } from "./core";
-  import AiSummarizer from "./components/AISummarizer.svelte";
+  import { RSSItem, RSSFeed } from "./core";
+  import nerdyXml from "./assets/nerdy.xml?raw";
 
-  const rawFeed = new RSSParser(rssFile).feed;
-  const rawFeed2 = new RSSParser(rssFile2).feed;
-  const rawFeed3 = new RSSParser(rssFile3).feed;
-  const feed: RSSNode[] = $state([]);
+  let feed: RSSItem[] = $state([...new RSSFeed(nerdyXml).feed]);
 
-  for (const element of [...rawFeed, ...rawFeed2, ...rawFeed3]) {
-    const item = new RSSNode(element);
-    feed.push(item);
+  const getFeeds = async () => {
+    const results: Record<string, RSSFeed> = await chrome.storage.local.get(null);
+    
+    const allFeeds = Object.values(results).flatMap((rssFeed: RSSFeed) => {
+      return rssFeed.feed
+    });
+    
+    feed = [...feed, ...allFeeds];
   }
 
-  feed.sort((a, b) => {
-    if (a.publishedAt && b.publishedAt) {
-      return b.publishedAt.getTime() - a.publishedAt.getTime();
-    }
-    return 0;
-  });
+  const sortFeed = () => {
+    feed.sort((a, b) => {
+      if (a.publishedAt && b.publishedAt) {
+        if (a.publishedAt < b.publishedAt) return -1;
+        else if (a.publishedAt > b.publishedAt) return 1;
+      }
+      return 0;
+    });
+  }
+
+  const initFeed = async () => {
+    await getFeeds();
+    sortFeed();
+  }
+
+  initFeed();
 
   // const opml = new OPMLParser("https://nerdy.dev/subscriptions.opml");
 </script>
