3b5ce5c8f9df3da2dc54767233721dd8c0214d58
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-29 18:02:09 +0100

    feat: add Chip component

---
+++ src/components/Chip.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  type ChipProps = {
+    title: string;
+    /** URL to an img shown as avatar */
+    src?: string;
+    /**
+     * If `onclick` is provided, the `<Chip>`
+     * is rendered as a `<button>`.
+     */
+    onclick?: () => void;
+  };
+
+  let props: ChipProps = $props();
+</script>
+
+{#snippet body({ src, title }: ChipProps)}
+  {#if src}
+    <img class="img" {src} alt={title} />
+  {/if}
+  <span class="title">{title}</span>
+{/snippet}
+
+{#if onclick}
+  <div class="chip">
+    {@render body(props)}
+  </div>
+{:else}
+  <button type="button" class="chip" onclick={props.onclick}>
+    {@render body(props)}
+  </button>
+{/if}
+
+<style>
+  .chip {
+    --size: 2rem;
+    --padding: var(--spacing-nearest);
+
+    display: inline-flex;
+    align-items: center;
+    gap: var(--spacing-near);
+    padding: var(--padding);
+    block-size: var(--size);
+    border-radius: var(--size);
+    border: 1px solid var(--color-border-base);
+    background: unset;
+  }
+
+  .img {
+    block-size: calc(var(--size) * 0.625);
+    aspect-ratio: 1/1;
+    border-radius: 100%;
+    object-fit: cover;
+    overflow: hidden;
+  }
+
+  .title {
+    --margin: var(--padding);
+
+    font-family: var(--font-family-sans);
+    font-size: var(--font-size-smallest);
+    font-weight: var(--font-weight-strong);
+    margin-inline-end: var(--margin);
+
+    &:only-child {
+      margin-inline-start: var(--margin);
+    }
+  }
+</style>
