354bd5f4f8ba2246773fb2cd1af30112f879634c
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-29 18:02:15 +0100

    feat: add Details component

---
+++ src/components/Details.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type SuggestionDetailsProps = {
+    name: string;
+    id?: string;
+    title: string;
+    content: Snippet;
+  };
+
+  let { name, id, title, content }: SuggestionDetailsProps = $props();
+</script>
+
+<details {name} {id}>
+  <summary>{title}</summary>
+  <div class="stack">{@render content()}</div>
+</details>
+
+<style>
+  .stack {
+    display: flex;
+    gap: var(--spacing-near);
+    flex-wrap: wrap;
+  }
+</style>
