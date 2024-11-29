2648c15b4d1850fb6b2d0b36edff21d59ca7a22c
Lukas Koeller <acc.koeller@protonmail.com>
2024-11-29 18:01:45 +0100

    refactor: abstract to AddButton

--- src/components/AddButton.svelte
+++ src/components/AddButton.svelte
@@ -1,49 +1,11 @@
-<script lang="ts">
-  import { RSSFeed } from "../core";
-
-  let formEl: HTMLFormElement;
-
-  const onSubmit = async (event: SubmitEvent) => {
-    event.preventDefault();
-    const formData = new FormData(formEl);
-    const url = formData.get("url") as string;
-
-    const response = await fetch(url);
-    const rawText = await response.text();
-    const rssItem = new RSSFeed(rawText);
-
-    // Add RSS Feed to Storage
-    await chrome.storage.local.set({ [url]: rssItem });
-  };
+<script>
+  import AddFeedForm from "./AddFeedForm.svelte";
 </script>
 
 <button class="import-button" popovertarget="import-button">Add RSS Feed</button
 >
-<div class="import-popup" id="import-button" popover onsubmit={onSubmit}>
-  <form class="form" bind:this={formEl}>
-    <!-- COMPONENT-START: nc-input-field -->
-    <div class="nc-input-field">
-      <label for="nc-input" class="label">
-        <div>
-          <div class="nc-input-label">URL</div>
-          <span class="nc-hint"
-            >Insert the URL of RSS Feed you would like to subscribe to</span
-          >
-        </div>
-      </label>
-      <input
-        id="url"
-        name="url"
-        class="nc-input"
-        aria-required="true"
-        autocomplete=""
-        type="url"
-        required
-      />
-    </div>
-    <!-- COMPONENT-END: nc-input-field -->
-    <button class="submit-button" type="submit">Add</button>
-  </form>
+<div class="import-popup" id="import-button" popover>
+  <AddFeedForm />
 </div>
 
 <style>
@@ -62,20 +24,5 @@
     padding: var(--spacing-base);
     border-radius: var(--border-radius-large);
     box-shadow: var(--shadow-elevation-medium);
-  }
-
-  .form {
-    display: grid;
-    gap: var(--spacing-base);
-  }
-
-  .label {
-    display: grid;
-    gap: var(--spacing-nearest);
-  }
-
-  .submit-button {
-    justify-self: end;
-    padding-inline: var(--spacing-far);
   }
 </style>
