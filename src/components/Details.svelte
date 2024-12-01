895fb9c6d2c9648d54939b1ce92c88b05ee1a6b1
Lukas Koeller <acc.koeller@protonmail.com>
2024-12-01 21:29:52 +0100

    chore: add className prop to Details

--- src/components/Details.svelte
+++ src/components/Details.svelte
@@ -7,12 +7,13 @@
     title: string;
     content: Snippet;
     open?: boolean;
+    className?: string;
   };
 
-  let { name, id, title, content, open }: SuggestionDetailsProps = $props();
+  let { name, id, title, content, open, className }: SuggestionDetailsProps = $props();
 </script>
 
-<details {name} {id} {open}>
+<details {name} {id} {open} class={className}>
   <summary>{title}</summary>
   <div>{@render content()}</div>
 </details>
