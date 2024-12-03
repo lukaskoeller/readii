2d5127003e7da5a5740a8177daf2c5e5bfe625d6
Lukas Koeller <acc.koeller@protonmail.com>
2024-12-03 08:09:27 +0100

    fix: increase contrast for text color in AISummarizer

--- src/components/AISummarizer.svelte
+++ src/components/AISummarizer.svelte
@@ -99,5 +99,6 @@
     background-color: var(--color-brand-secondary-base);
     box-shadow: 0px 2px 0px 0px var(--color-brand-secondary-emphasis);
     margin-block: var(--spacing-base);
+    color: var(--color-text-on-emphasis-dark);
   }
 </style>
