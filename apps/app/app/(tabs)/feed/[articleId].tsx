1295f8318d16fc4b67e37de1ff8fddc62f6fb713
Lukas Koeller <acc.koeller@protonmail.com>
2025-06-20 23:25:56 +0200

    chore: improve padding

--- apps/app/app/(tabs)/feed/[articleId].tsx
+++ apps/app/app/(tabs)/feed/[articleId].tsx
@@ -5,11 +5,7 @@ import { Spacing } from "@/constants/Sizes";
 import { useItem } from "@/hooks/queries";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
 import { useLocalSearchParams } from "expo-router";
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-} from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import { parse } from "parse5";
 
 export default function Article() {
@@ -20,12 +16,17 @@ export default function Article() {
 
   return (
     <SafeAreaView>
-      <ThemedView padding={Spacing.size3} style={styles.flow}>
-        <ScrollView style={{ marginBlockEnd: Spacing.size8, marginBlockStart: Spacing.size3 }}>
-          <ThemedView>
-            <ThemedText type="title">{data?.title}</ThemedText>
+      <ThemedView style={styles.flow}>
+        <ScrollView style={{ padding: Spacing.size3 }}>
+          <ThemedView
+            style={{
+              marginBlockEnd: Spacing.size8,
+              marginBlockStart: Spacing.size3,
+            }}
+          >
+            <ThemedText type="h1">{data?.title}</ThemedText>
+            <HtmlViewer ast={contentAst} />
           </ThemedView>
-          <HtmlViewer ast={contentAst} />
         </ScrollView>
       </ThemedView>
     </SafeAreaView>
