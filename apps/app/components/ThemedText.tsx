19b829cf100174b410f89c70789b51ce38309dc9
Lukas Koeller <acc.koeller@protonmail.com>
2025-06-20 23:26:24 +0200

    feat: add proper styling for ThemedText

--- apps/app/components/ThemedText.tsx
+++ apps/app/components/ThemedText.tsx
@@ -1,33 +1,47 @@
-import { StyleSheet, Text, type TextProps, Platform } from 'react-native';
+import { StyleSheet, Text, type TextProps, Platform } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Radius } from "@/constants/Sizes";
+import { H1_STYLE, H2_STYLE, H3_STYLE, H4_STYLE, H5_STYLE, H6_STYLE, TEXT_DEFAULT_STYLE } from "@/constants/Styles";
+import { useTextColor } from "@/hooks/useTextColor";
+
+export type TThemedTextType =
+  | "default"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "code";
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link' | 'code';
+  type?: TThemedTextType;
 };
 
 export function ThemedText({
   style,
   lightColor,
   darkColor,
-  type = 'default',
+  type = "default",
   ...rest
 }: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const color = useTextColor(type);
+  const colorBackground2 = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background2"
+  );
+  const baseStyles = styles[type];
 
   return (
     <Text
       style={[
         { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        type === 'code' ? styles.code : undefined,
+        baseStyles,
         style,
+        type === "code" ? { backgroundColor: colorBackground2 } : {},
       ]}
       {...rest}
     />
@@ -35,34 +49,21 @@ export function ThemedText({
 }
 
 const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
+  default: TEXT_DEFAULT_STYLE,
+  h1: H1_STYLE,
+  h2: H2_STYLE,
+  h3: H3_STYLE,
+  h4: H4_STYLE,
+  h5: H5_STYLE,
+  h6: H6_STYLE,
   code: {
-    fontFamily: Platform.select({ ios: 'Menlo', android: 'monospace', default: 'monospace' }),
-    fontSize: 16,
-    backgroundColor: '#eee',
+    fontFamily: Platform.select({
+      ios: "Menlo",
+      android: "monospace",
+      default: "monospace",
+    }),
     padding: 2,
-    borderRadius: 4,
+    borderRadius: Radius.size3,
+    ...TEXT_DEFAULT_STYLE,
   },
 });
