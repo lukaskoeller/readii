508a9e9f0248eed0a30995dc68092579f805d3bc
Lukas Koeller <acc.koeller@protonmail.com>
2025-06-22 21:22:03 +0200

    chore: add color prop to useTextColor

--- apps/app/hooks/useTextColor.ts
+++ apps/app/hooks/useTextColor.ts
@@ -1,11 +1,16 @@
 import { TThemedTextType } from "@/components/ThemedText";
 import { useThemeColor } from "./useThemeColor";
 
-export const useTextColor = (type: TThemedTextType) => {
+export const useTextColor = (type: TThemedTextType, color?: "text" | "text2") => {
   const colorText = useThemeColor({}, "text");
   const colorText2 = useThemeColor({}, "text2");
 
+  if (color === "text2") {
+    return colorText2;
+  }
+
   if (
+    color === "text" ||
     type === "h1" ||
     type === "h2" ||
     type === "h3" ||
