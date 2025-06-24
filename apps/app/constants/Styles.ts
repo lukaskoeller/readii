fd066f87f0d6ab2db1a969b7ed855004d0da15e0
Lukas Koeller <acc.koeller@protonmail.com>
2025-06-24 22:52:44 +0200

    chore: improve text styles

--- apps/app/constants/Styles.ts
+++ apps/app/constants/Styles.ts
@@ -3,24 +3,27 @@ import { FontSize, FontWeight, Spacing } from "./Sizes";
 export const H1_STYLE = {
   fontSize: FontSize.size8,
   fontWeight: FontWeight.bold,
-  marginBlock: Spacing.size3,
+  marginBlockStart: Spacing.size5,
+  marginBlockEnd: Spacing.size2,
 } as const;
 export const H2_STYLE = {
   fontSize: FontSize.size7,
   fontWeight: FontWeight.bold,
-  marginBlock: Spacing.size3,
-  paddingBlock: Spacing.size1,
+  marginBlockStart: Spacing.size5,
+  marginBlockEnd: Spacing.size2,
   lineHeight: FontSize.size7 * 1.3,
 } as const;
 export const H3_STYLE = {
   fontSize: FontSize.size6,
   fontWeight: FontWeight.bold,
-  marginBlock: Spacing.size2,
+  marginBlockStart: Spacing.size4,
+  marginBlockEnd: Spacing.size2,
 } as const;
 export const H4_STYLE = {
   fontSize: FontSize.size4,
   fontWeight: FontWeight.bold,
-  marginBlock: Spacing.size2,
+  marginBlockStart: Spacing.size3,
+  marginBlockEnd: Spacing.size2,
 } as const;
 export const H5_STYLE = {
   fontSize: FontSize.size3,
@@ -33,6 +36,7 @@ export const H6_STYLE = {
   marginBlock: Spacing.size2,
 } as const;
 export const TEXT_DEFAULT_STYLE = {
-  fontSize: FontSize.size2,
-  lineHeight: FontSize.size2 * 1.5,
+  fontSize: 14*1.2,
+  lineHeight: FontSize.size3 * 1.5,
+  marginBlock: Spacing.size2,
 } as const;
