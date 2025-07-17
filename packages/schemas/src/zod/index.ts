0c65430bb5e348a89b85bbd9fffec14d1a924bb2
Lukas Koeller <acc.koeller@protonmail.com>
2025-07-18 00:25:30 +0200

    feat(schemas): add new package schemas

---
+++ packages/schemas/src/zod/index.ts
@@ -0,0 +1,51 @@
+import { url, z } from "zod/mini";
+
+const $DatabaseId = z.optional(z.int().check(z.positive()));
+
+export const $HttpsUrl = z.string().check(
+  z.url({
+    protocol: /^https?$/,
+  })
+);
+
+const $IsoDatetime = z.iso.datetime();
+
+export const $MediaSourceIcon = z.object({
+  id: $DatabaseId,
+  title: z.string(),
+  url: $HttpsUrl,
+  // mediaSourceId is not included here as it's a foreign key reference
+});
+
+export const $MediaSource = z.object({
+  id: $DatabaseId,
+  name: z.string(),
+  description: z.nullable(z.string()),
+  url: $HttpsUrl,
+  feedUrl: $HttpsUrl,
+  logoUrl: $HttpsUrl,
+  lastBuildAt: z.nullable($IsoDatetime),
+  lastFetchedAt: $IsoDatetime,
+  language: z.nullable(z.string()),
+  generator: z.nullable(z.string()),
+  categories: z.nullable(z.array(z.string())),
+});
+
+export const $MediaType = z.enum(["text", "audio", "video"]);
+
+export const $MediaItem = z.object({
+  id: $DatabaseId,
+  title: z.string(),
+  type: $MediaType,
+  url: $HttpsUrl,
+  content: z.string(), // most important field
+  contentSnippet: z.nullable(z.string()),
+  contentTldr: z.nullable(z.string()),
+  creator: z.nullable(z.string()),
+  publishedAt: $IsoDatetime,
+  thumbnailUrl: z.nullable($HttpsUrl),
+  enclosure: z.nullable($HttpsUrl),
+  isStarred: z._default(z.boolean(), false),
+  isRead: z._default(z.boolean(), false),
+  isReadLater: z._default(z.boolean(), false),
+});
