4e55aa5aff6f8e8e711204bd5a00115fa4809979
Lukas Koeller <acc.koeller@protonmail.com>
2025-07-20 18:26:42 +0200

    feat: add <Link> to LinkListCard

--- apps/app/components/LinkListCard.tsx
+++ apps/app/components/LinkListCard.tsx
@@ -3,6 +3,7 @@ import { Radius, Spacing } from "@/constants/Sizes";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { ThemedView } from "./ThemedView";
 import { ListItem } from "./ListItem";
+import { Link } from "expo-router";
 
 export type LinkListCardProps = {
   data: {
@@ -18,12 +19,19 @@ export function LinkListCard({ data }: LinkListCardProps) {
   return (
     <ThemedView style={[styles.list, { backgroundColor: colorBackground3 }]}>
       {data.map((item, idx, array) => (
-        <ListItem
-          label={item.label}
-          icon={item.icon}
-          isLastItem={idx === array.length - 1}
+        <Link
           key={item.id}
-        />
+          href={{
+            pathname: "/feed",
+            params: { mediaSourceId: item.id },
+          }}
+        >
+          <ListItem
+            label={item.label}
+            icon={item.icon}
+            isLastItem={idx === array.length - 1}
+          />
+        </Link>
       ))}
     </ThemedView>
   );
