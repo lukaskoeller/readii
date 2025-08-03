0db9bfa179922d73e6aceee32843481fec9da537
Lukas Koeller <acc.koeller@protonmail.com>
2025-08-03 23:14:24 +0200

    feat(app): use href prop in LinkListCard

--- apps/app/components/LinkListCard.tsx
+++ apps/app/components/LinkListCard.tsx
@@ -3,13 +3,14 @@ import { Radius, Spacing } from "@/constants/Sizes";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { ThemedView } from "./ThemedView";
 import { ListItem } from "./ListItem";
-import { Link } from "expo-router";
+import { Link, LinkProps } from "expo-router";
 
 export type LinkListCardProps = {
   data: {
     id: string;
     label: string;
     icon: React.JSX.Element;
+    href: LinkProps["href"];
   }[];
 };
 
@@ -19,13 +20,7 @@ export function LinkListCard({ data }: LinkListCardProps) {
   return (
     <ThemedView style={[styles.list, { backgroundColor: colorBackground3 }]}>
       {data.map((item, idx, array) => (
-        <Link
-          key={item.id}
-          href={{
-            pathname: "/feed",
-            params: { mediaSourceId: item.id, feedTitle: item.label },
-          }}
-        >
+        <Link key={item.id} href={item.href}>
           <ListItem
             label={item.label}
             icon={item.icon}
