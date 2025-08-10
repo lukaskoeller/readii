890bc01baf7b48f5b63062775dcfdc3740ad2c77
Lukas Koeller <acc.koeller@protonmail.com>
2025-08-10 17:16:30 +0200

    chore: move add to directory

--- apps/app/app/(tabs)/index/add/index.tsx
+++ apps/app/app/(tabs)/index/add/index.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet, View, Platform, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Platform,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { ThemedText } from "@/components/ThemedText";
 import { FontSize, Radius, Spacing } from "@/constants/Sizes";
@@ -14,6 +22,9 @@ import * as Clipboard from "expo-clipboard";
 import { Collapsible } from "@/components/Collapsible";
 import { Card } from "@/components/Card";
 import { Section } from "@/components/Section";
+import { LinkList } from "@/components/LinkList";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { SimpleCardLink } from "@/components/SimpleCardLink";
 
 export type TFeedPreview = {
   iconUrl: string | null;
@@ -31,12 +42,77 @@ export default function AddFeed() {
   const colorBackground2 = useThemeColor({}, "background2");
   const colorBorder = useThemeColor({}, "border");
   const colorPrimary = useThemeColor({}, "primary");
+  const colorText2 = useThemeColor({}, "text2");
 
   return (
     <View style={[styles.container, { borderColor: colorBorder }]}>
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
-      <Section title="Browse Categories" href="/add/browse-categories">
-        <ThemedText>Categories</ThemedText>
+      <LinkList
+        style={{ margin: Spacing.container }}
+        data={[
+          {
+            href: "/add/feed",
+            label: "Add Feed",
+            icon: (
+              <IconSymbol name="link" size={Spacing.size5} color={colorText2} />
+            ),
+          },
+          {
+            href: "/add/collection",
+            label: "Add Collection",
+            icon: (
+              <IconSymbol
+                name="rectangle.stack"
+                size={Spacing.size5}
+                color={colorText2}
+              />
+            ),
+          },
+          {
+            href: "/add/opml",
+            label: "Add from OPML",
+            icon: (
+              <IconSymbol
+                name="smallcircle.fill.circle"
+                size={Spacing.size5}
+                color={colorText2}
+              />
+            ),
+          },
+        ]}
+      />
+      <Section title="Browse Categories" href="/add/categories">
+        <FlatList
+          data={[
+            { href: "/add/categories/news-politics", label: "News & Politics" },
+            { href: "/add/categories/entertainment", label: "Entertainment" },
+            { href: "/add/categories/sports", label: "Sports" },
+            {
+              href: "/add/categories/money-business",
+              label: "Money & Business",
+            },
+            { href: "/add/categories/style-beauty", label: "Style & Beauty" },
+            { href: "/add/categories/food", label: "Food" },
+            {
+              href: "/add/categories/travel-regional",
+              label: "Travel & Regional",
+            },
+            { href: "/add/categories/health", label: "Health" },
+            { href: "/add/categories/home-garden", label: "Home & Garden" },
+            { href: "/add/categories/science-tech", label: "Science & Tech" },
+            { href: "/add/categories/cars", label: "Cars" },
+            { href: "/add/categories/hobbies", label: "Hobbies" },
+            { href: "/add/categories/outdoors", label: "Outdoors" },
+            {
+              href: "/add/categories/kids-parenting",
+              label: "Kids & Parenting",
+            },
+          ]}
+          renderItem={({ item }) => (
+            <SimpleCardLink href={item.href} label={item.label} />
+          )}
+          horizontal
+        />
       </Section>
       <ThemedView style={[styles.preview, { borderColor: colorBorder }]}>
         {feedPreview ? (
@@ -70,36 +146,34 @@ export default function AddFeed() {
         )}
       </ThemedView>
       <Card>
-        <Collapsible title="File-based routing">
-          <ThemedView>
-            <ThemedText>URL</ThemedText>
-            <TextInput
-              style={[
-                styles.input,
-                { backgroundColor: colorBackground2, borderColor: colorBorder },
-              ]}
-              inputMode="url"
-              onChangeText={async (text) => {
-                setFeedUrl(text);
-                const feedUrl = $HttpsUrl.safeParse(text);
-                if (feedUrl.success) {
-                  const feedData = await getFeedData(feedUrl.data);
-                  setFeedPreview({
-                    iconUrl: feedData.mediaSourceIcon.url,
-                    name: feedData.mediaSource.name,
-                    url: feedData.mediaSource.url,
-                    description: feedData.mediaSource.description,
-                    mediaItemsCount: feedData.mediaItems.length,
-                  });
-                  console.log(feedData);
-                } else {
-                  setFeedPreview(null);
-                }
-              }}
-              value={feedUrl}
-            />
-          </ThemedView>
-        </Collapsible>
+        <ThemedView>
+          <ThemedText>URL</ThemedText>
+          <TextInput
+            style={[
+              styles.input,
+              { backgroundColor: colorBackground2, borderColor: colorBorder },
+            ]}
+            inputMode="url"
+            onChangeText={async (text) => {
+              setFeedUrl(text);
+              const feedUrl = $HttpsUrl.safeParse(text);
+              if (feedUrl.success) {
+                const feedData = await getFeedData(feedUrl.data);
+                setFeedPreview({
+                  iconUrl: feedData.mediaSourceIcon.url,
+                  name: feedData.mediaSource.name,
+                  url: feedData.mediaSource.url,
+                  description: feedData.mediaSource.description,
+                  mediaItemsCount: feedData.mediaItems.length,
+                });
+                console.log(feedData);
+              } else {
+                setFeedPreview(null);
+              }
+            }}
+            value={feedUrl}
+          />
+        </ThemedView>
       </Card>
       <ThemedView>
         <Button
@@ -134,7 +208,9 @@ export default function AddFeed() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: Spacing.size4,
+    display: "flex",
+    flexDirection: "column",
+    gap: Spacing.size4,
   },
   input: {
     borderRadius: Radius.size3,
@@ -170,5 +246,12 @@ const styles = StyleSheet.create({
     gap: Spacing.size2,
     marginBlock: Spacing.size3,
     marginInlineEnd: Spacing.size3,
+  },
+  gallery: {
+    display: "flex",
+    flexDirection: "row",
+    gap: Spacing.size3,
+    rowGap: Spacing.size3,
+    columnGap: Spacing.size3,
   },
 });
