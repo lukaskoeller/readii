a804ba9f8aee952ad953eef33b94f8620453f948
Lukas Koeller <acc.koeller@protonmail.com>
2025-08-13 22:26:48 +0200

    feat(app): add container prop to ThemedView

--- apps/app/components/ThemedView.tsx
+++ apps/app/components/ThemedView.tsx
@@ -1,11 +1,25 @@
-import { View, type ViewProps } from 'react-native';
+import { Spacing } from "@/constants/Sizes";
+import { View, type ViewProps } from "react-native";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
   padding?: number;
+  container?: boolean;
 };
 
-export function ThemedView({ padding, style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  return <View style={[style, { padding }]} {...otherProps} />;
+export function ThemedView({
+  padding,
+  style,
+  lightColor,
+  darkColor,
+  container,
+  ...otherProps
+}: ThemedViewProps) {
+  return (
+    <View
+      style={[style, { padding: container ? Spacing.container : padding }]}
+      {...otherProps}
+    />
+  );
 }
