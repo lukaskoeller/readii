764e7d76c441f1411e283fa846270c7a297e9a70
Lukas Koeller <acc.koeller@protonmail.com>
2025-09-19 23:51:41 +0200

    feat(web): add Feed component

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Feed from '$lib/components/Feed.svelte';
 	import LanguageSwitch from '$lib/components/LanguageSwitch.svelte';
 	import Logo from '$lib/components/Logo.svelte';
 	import { m } from '$lib/paraglide/messages';
@@ -10,32 +11,10 @@
 </header>
 <main>
 	<p class="text">{m.intro()}</p>
-	<p class="text">{m.waitlist_paragraph()}</p>
-	<form>
-		<!-- COMPONENT-START: nc-input-field -->
-		<div class="nc-input-field">
-			<label for="input" class="nc-stack">
-				<span class="nc-input-label">{m.form_waitlist_email_input()}</span>
-			</label>
-			<input
-				id="input"
-				class="nc-input input"
-				aria-required="true"
-				autocomplete="email"
-				type="email"
-				required
-			/>
-		</div>
-		<!-- COMPONENT-END: nc-input-field -->
-		<button class="nc-button -primary submit-button" type="submit"
-			>{m.form_waitlist_submit_label()}</button
-		>
-	</form>
+	<Feed />
 </main>
 
 <style>
-	@custom-media --motionOK (prefers-reduced-motion: no-preference);
-
 	header {
 		display: flex;
 		justify-content: space-between;
@@ -45,15 +24,5 @@
 
 	.text {
 		margin-block: var(--spacing-far);
-	}
-
-	.input {
-		font-size: var(--font-size-display);
-		padding-block: var(--spacing-base);
-		block-size: 1lh;
-	}
-
-	.submit-button {
-		margin-block-start: var(--spacing-base);
 	}
 </style>
