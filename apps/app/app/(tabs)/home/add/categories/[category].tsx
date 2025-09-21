7c20158744184a8e58c2a72c0c6f105bb510e176
Lukas Koeller <acc.koeller@protonmail.com>
2025-09-21 22:46:23 +0200

    feat(app): add category screen

---
+++ apps/app/app/(tabs)/home/add/categories/[category].tsx
@@ -0,0 +1,46 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Spacing } from "@/constants/Sizes";
+import { FlatList } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export default function Category() {
+  const backgroundColor = useThemeColor({}, "background");
+  const params = useLocalSearchParams<{ category: string }>();
+
+  return (
+    <ThemedView style={{ backgroundColor }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor,
+          },
+          headerTitle: params.category,
+        }}
+      />
+      <FlatList
+        data={[
+          {
+            title: "Example Feed",
+            url: "https://example.com/feed",
+            description: "This is an example feed description.",
+          },
+          {
+            title: "Another Feed",
+            url: "https://another.com/feed",
+            description: null,
+          },
+          {
+            title: "Third Feed",
+            url: "https://third.com/feed",
+            description: "This is the third feed description.",
+          },
+        ]}
+        keyExtractor={(item) => item.url}
+        renderItem={({ item }) => <ThemedText>{item.title}</ThemedText>}
+        contentContainerStyle={{ paddingBottom: Spacing.navigation }}
+      />
+    </ThemedView>
+  );
+}
