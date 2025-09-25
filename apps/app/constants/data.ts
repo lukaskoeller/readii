35a8691aad392790a31be9f5dad7aa00b20185ef
Lukas Koeller <acc.koeller@protonmail.com>
2025-09-26 00:14:23 +0200

    feat(app): add categories data

---
+++ apps/app/constants/data.ts
@@ -0,0 +1,114 @@
+export const CATEGORIES = [
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "news-politics" }
+        },
+        key: "news-politics",
+        label: "News & Politics"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "entertainment" }
+        },
+        key: "entertainment",
+        label: "Entertainment"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "sports" }
+        },
+        key: "sports",
+        label: "Sports"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "money-business" }
+        },
+        key: "money-business",
+        label: "Money & Business"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "style-beauty" }
+        },
+        key: "style-beauty",
+        label: "Style & Beauty"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "food" }
+        },
+        key: "food",
+        label: "Food"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "travel-regional" }
+        },
+        key: "travel-regional",
+        label: "Travel & Regional"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "health" }
+        },
+        key: "health",
+        label: "Health"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "home-garden" }
+        },
+        key: "home-garden",
+        label: "Home & Garden"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "science-tech" }
+        },
+        key: "science-tech",
+        label: "Science & Tech"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "cars" }
+        },
+        key: "cars",
+        label: "Cars"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "hobbies" }
+        },
+        key: "hobbies",
+        label: "Hobbies"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "outdoors" }
+        },
+        key: "outdoors",
+        label: "Outdoors"
+    },
+    {
+        href: {
+            pathname: "/home/add/[category]",
+            params: { category: "kids-parenting" }
+        },
+        key: "kids-parenting",
+        label: "Kids & Parenting"
+    }
+] as const;
