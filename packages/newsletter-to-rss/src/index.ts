68b28c9889ca34b8d4d71496f7e7caa5dd90b29d
Lukas Koeller <acc.koeller@protonmail.com>
2025-09-30 22:05:16 +0200

    chore: add db init and get more data from parsed email

--- packages/newsletter-to-rss/src/index.ts
+++ packages/newsletter-to-rss/src/index.ts
@@ -1,6 +1,10 @@
 import { SMTPServer } from "smtp-server";
 import { simpleParser } from "mailparser";
-import { PORT } from "./constants.ts";
+import { PORT_SMTP } from "./constants.ts";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+
+const db = drizzle(process.env.DB_FILE_NAME!);
 
 // By default, the SMTP server listens for emails sent to any address at the host/IP and port specified.
 // You can send emails to anything@localhost:5425 or anything@[your-ip]:5425
@@ -9,8 +13,11 @@ const server = new SMTPServer({
 	onData(stream, _session, callback: (err?: Error | null) => void) {
 		simpleParser(stream)
 			.then((parsed) => {
-				console.log("Subject:", parsed.subject);
-                parsed.html
+				const subject = parsed.subject;
+                const date = parsed.date;
+				const content = parsed.html || parsed.textAsHtml || parsed.text;
+				console.log(`Subject: ${subject}; Date: ${date}; Content: ${content && content.slice(0, 100)}`);
+				// @todo Add item to sqlite database
 				callback();
 			})
 			.catch((err) => {
@@ -20,8 +27,8 @@ const server = new SMTPServer({
 	}
 });
 
-server.listen(PORT, () => {
-	console.log(`SMTP server listening on port ${PORT}`);
+server.listen(PORT_SMTP, () => {
+	console.log(`SMTP server listening on port ${PORT_SMTP}`);
 });
 
 export { server };
