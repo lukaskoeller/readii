916b6c7a345992d4dce45269117e71a9dab1ea73
Lukas Koeller <acc.koeller@protonmail.com>
2025-10-02 22:42:43 +0200

    feat(app): add delete function for feeds

--- apps/app/components/feed/HeaderActions.tsx
+++ apps/app/components/feed/HeaderActions.tsx
@@ -3,6 +3,8 @@ import { IconSymbol } from "../ui/IconSymbol";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Spacing } from "@/constants/Sizes";
 import { Pressable } from "react-native";
+import { useMediaSource } from "@/hooks/queries";
+import { useRouter } from "expo-router";
 
 export type HeaderActionsProps = {
   feedTitle: string;
@@ -11,6 +13,8 @@ export type HeaderActionsProps = {
 export function HeaderActions({
   feedTitle,
 }: HeaderActionsProps) {
+  const router = useRouter();
+  const { deleteMediaSource } = useMediaSource();
   const primaryColor = useThemeColor({}, "primary");
 
   return (
@@ -27,7 +31,10 @@ export function HeaderActions({
           paddingInline: Spacing.size3,
         }}
         aria-label="Delete Feed"
-        onPress={async () => feedTitle}
+        onPress={async () => {
+          deleteMediaSource(feedTitle, "name")
+          router.replace("/home");
+        }}
       >
         <IconSymbol
           name={"trash"}
