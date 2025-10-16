e6a0060306281192aff85496835850a028ef55e0
Lukas Koeller <acc.koeller@protonmail.com>
2025-10-16 23:40:11 +0200

    feat(app): add Link to FolderButton

--- apps/app/components/FolderButton.tsx
+++ apps/app/components/FolderButton.tsx
@@ -7,93 +7,105 @@ import { IconSymbol } from "./ui/IconSymbol";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Card } from "./Card";
 import { Image } from "expo-image";
+import { Link, LinkProps } from "expo-router";
 
 const THUMBNAIL_PREVIEW_COUNT = 4 as const;
 const MAIN_PADDING = Spacing.size4;
 const THUMBNAIL_BORDER_WIDTH = 2 as const;
 const THUMBNAIL_BORDER_COMPENSATION = THUMBNAIL_BORDER_WIDTH * 2;
-const FOOTER_ITEM_SIZE = Spacing.size5 + THUMBNAIL_BORDER_COMPENSATION;
+const FOOTER_ITEM_SIZE = Spacing.size4 + THUMBNAIL_BORDER_COMPENSATION;
 
 export type FolderButtonProps = {
   label: string;
   icon?: React.JSX.Element;
   count?: number;
   thumbnailUrls: string[];
+  href: LinkProps["href"];
 };
 
 export const FolderButton: FC<FolderButtonProps> = (props) => {
-  const { label, count, thumbnailUrls } = props;
+  const { label, count, thumbnailUrls, href } = props;
   const colorBackground = useThemeColor({}, "background");
   const colorBackground3 = useThemeColor({}, "background3");
+  const colorText2 = useThemeColor({}, "text2");
   const icon = props.icon ?? <IconSymbol name="folder" size={FontSize.size5} />;
   const additionalMediaSourcesCount =
     thumbnailUrls.length - THUMBNAIL_PREVIEW_COUNT;
 
   return (
-    <Card padding={false} style={styles.card}>
-      <ThemedView style={[styles.item]}>
-        <ThemedView style={[styles.icon]}>{icon}</ThemedView>
-        <ThemedView style={[styles.row]}>
-          <ThemedText
-            numberOfLines={1}
-            style={styles.text}
-            ellipsizeMode="tail"
-          >
-            {label}
-          </ThemedText>
-          <ThemedView style={styles.right}>
-            <IconSymbol
-              name="chevron.right"
-              color={colorBackground}
-              size={Spacing.size3}
-            />
-          </ThemedView>
-        </ThemedView>
-      </ThemedView>
-      <ThemedView style={styles.footer}>
-        {thumbnailUrls.length > 0 && (
-          <ThemedView style={styles.thumbnails}>
-            {thumbnailUrls.slice(0, THUMBNAIL_PREVIEW_COUNT).map((url, idx) => (
-              <ThemedView
-                key={url}
-                style={[
-                  styles.thumbnail,
-                  {
-                    backgroundColor: colorBackground,
-                    borderColor: colorBackground3,
-                  },
-                  idx === 0 && { marginInlineStart: 0 }
-                ]}
-              >
-                <Image
-                  source={{ uri: url }}
-                  style={{ width: "100%", height: "100%" }}
-                />
-              </ThemedView>
-            ))}
-            {additionalMediaSourcesCount > 0 && (
+    <Link href={href}>
+      <Card padding={false} style={styles.card}>
+        <ThemedView style={[styles.item]}>
+          <ThemedView style={[styles.icon]}>{icon}</ThemedView>
+          <ThemedView style={[styles.row]}>
+            <ThemedText
+              numberOfLines={1}
+              style={styles.text}
+              ellipsizeMode="tail"
+            >
+              {label}
+            </ThemedText>
+            <ThemedView style={styles.right}>
               <ThemedView
                 style={[
-                  styles.thumbnailCount,
-                  {
-                    backgroundColor: colorBackground,
-                    borderColor: colorBackground3,
-                  },
+                  styles.unreadBadge,
+                  { backgroundColor: colorBackground },
                 ]}
               >
-                <ThemedText type="small">
-                  {`+ ${additionalMediaSourcesCount}`}
-                </ThemedText>
+                <ThemedText type="small">{count ?? 0}</ThemedText>
               </ThemedView>
-            )}
+              <IconSymbol
+                name="chevron.right"
+                color={colorText2}
+                size={Spacing.size3}
+              />
+            </ThemedView>
           </ThemedView>
-        )}
-        <ThemedView style={styles.unreadBadge}>
-          <IconSymbol name="app.badge" size={FOOTER_ITEM_SIZE - THUMBNAIL_BORDER_COMPENSATION} />
-          <ThemedText type="small">{count ?? 0}</ThemedText>
         </ThemedView>
-      </ThemedView>
-    </Card>
+        <ThemedView style={styles.footer}>
+          {thumbnailUrls.length > 0 && (
+            <ThemedView style={styles.thumbnails}>
+              {thumbnailUrls
+                .slice(0, THUMBNAIL_PREVIEW_COUNT)
+                .map((url, idx) => (
+                  <ThemedView
+                    key={url}
+                    style={[
+                      styles.thumbnail,
+                      {
+                        backgroundColor: colorBackground,
+                        outlineColor: colorBackground3,
+                      },
+                      idx === 0 && { marginInlineStart: 0 },
+                    ]}
+                  >
+                    <Image
+                      source={{ uri: url }}
+                      style={{ width: "100%", height: "100%" }}
+                      contentFit="contain"
+                    />
+                  </ThemedView>
+                ))}
+              {additionalMediaSourcesCount > 0 && (
+                <ThemedView
+                  style={[
+                    styles.thumbnailCount,
+                    {
+                      backgroundColor: colorBackground,
+                      outlineColor: colorBackground,
+                    },
+                  ]}
+                >
+                  <ThemedText type="small">
+                    {`+${additionalMediaSourcesCount}`}
+                  </ThemedText>
+                </ThemedView>
+              )}
+            </ThemedView>
+          )}
+        </ThemedView>
+      </Card>
+    </Link>
   );
 };
 
@@ -144,8 +156,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: Spacing.size5,
-    justifyContent: "space-between",
-    marginBlockEnd: MAIN_PADDING / 2,
+    marginInlineEnd: MAIN_PADDING,
+    justifyContent: "flex-start",
+    marginBlockEnd: MAIN_PADDING,
+    borderBlockStartColor: "lightgray",
   },
   thumbnails: {
     flexDirection: "row",
@@ -155,8 +169,8 @@ const styles = StyleSheet.create({
     width: FOOTER_ITEM_SIZE,
     height: FOOTER_ITEM_SIZE,
     borderRadius: Radius.full,
-    marginInlineStart: Spacing.size3 * -1,
-    borderWidth: THUMBNAIL_BORDER_WIDTH,
+    marginInlineStart: 10 * -1,
+    outlineWidth: THUMBNAIL_BORDER_WIDTH,
     overflow: "hidden",
   },
   thumbnailCount: {
@@ -164,16 +178,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     height: FOOTER_ITEM_SIZE,
-    borderWidth: THUMBNAIL_BORDER_WIDTH,
-    paddingInline: Spacing.size1,
+    outlineWidth: THUMBNAIL_BORDER_WIDTH,
+    paddingInline: Spacing.size3 / 2,
     borderRadius: Radius.full,
-    marginInlineStart: Spacing.size3 * -1,
+    marginInlineStart: 10 * -1,
   },
   unreadBadge: {
-    paddingInlineEnd: MAIN_PADDING,
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
     gap: Spacing.size1,
-  }
+    paddingBlock: Spacing.size1 / 2,
+    paddingInline: Spacing.size2,
+    borderRadius: Radius.full,
+  },
 });
