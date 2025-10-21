1340ed04a9c74216be78ebe5eb091117f3be0ce9
Lukas Koeller <acc.koeller@protonmail.com>
2025-10-21 22:59:23 +0200

    test(parser): add test for atproto feed

--- packages/parser/tests/index.test.ts
+++ packages/parser/tests/index.test.ts
@@ -1,19 +1,30 @@
 import { expect, test } from "vitest";
-import { getFeedData } from "../src";
+import { getFeedData, TMediaSourceType } from "../src";
 
-const feeds = [
-  ["Adam Argyle", "https://nerdy.dev/rss"],
-  ["Contrario by Victor Matekole", "https://matekole.com/rss.xml"],
-  ["NYT Top Stories", "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"],
-  ["Jake Archibald", "https://jakearchibald.com/posts.rss"],
-  ["Smashing Magazine", "https://www.smashingmagazine.com/feed/"],
+const rssFeeds: Array<[string, TMediaSourceType, string]> = [
+  ["Adam Argyle", "rss", "https://nerdy.dev/rss"],
+  ["Contrario by Victor Matekole", "rss", "https://matekole.com/rss.xml"],
+  [
+    "NYT Top Stories",
+    "rss",
+    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+  ],
+  ["Jake Archibald", "rss", "https://jakearchibald.com/posts.rss"],
+  ["Smashing Magazine", "rss", "https://www.smashingmagazine.com/feed/"],
+  [
+    "readii.bsky.social",
+    "atproto",
+    "https://api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?filter=posts_no_replies&actor=did:plc:fmwrdogxjglnbwalvoe6jdam&limit=100",
+  ],
 ];
 
-
-test.each(feeds)("should parse RSS feed \"%s\" correctly", async (title, url) => {
-  const parsedFeed = await getFeedData(url);
-  console.log(`[${title}] mediaSourceIcon:`, parsedFeed.mediaSourceIcon);
-  expect("mediaSourceIcon" in parsedFeed).toBe(true);
-  expect("mediaSource" in parsedFeed).toBe(true);
-  expect("mediaItems" in parsedFeed).toBe(true);
-});
+test.each(rssFeeds)(
+  'should parse feed "%s" (%s) correctly',
+  async (title, source, url) => {
+    const parsedFeed = await getFeedData(url, { source });
+    console.log(`[${title}] mediaSourceIcon:`, parsedFeed.mediaSourceIcon);
+    expect("mediaSourceIcon" in parsedFeed).toBe(true);
+    expect("mediaSource" in parsedFeed).toBe(true);
+    expect("mediaItems" in parsedFeed).toBe(true);
+  }
+);
