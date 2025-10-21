3013fe9585a352d5f4f87f8205a5829865f7f5ee
Lukas Koeller <acc.koeller@protonmail.com>
2025-10-21 23:00:25 +0200

    chore(web): use bsky api for readii feed retrieval

--- apps/web/src/lib/constants/index.ts
+++ apps/web/src/lib/constants/index.ts
@@ -1,1 +1,1 @@
-export const FEED_URL = 'https://bsky.app/profile/readii.bsky.social/rss';
+export const FEED_URL = 'https://api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?filter=posts_no_replies&actor=did:plc:fmwrdogxjglnbwalvoe6jdam&limit=100';
