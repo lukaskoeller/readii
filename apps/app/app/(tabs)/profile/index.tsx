53d69168507528821920774f6d8e0593d1ddda51
Lukas Koeller <acc.koeller@protonmail.com>
2025-10-25 00:56:00 +0200

    feat(app): add pricing to profile page

--- apps/app/app/(tabs)/profile/index.tsx
+++ apps/app/app/(tabs)/profile/index.tsx
@@ -1,14 +1,19 @@
 import { Card } from "@/components/Card";
 import { LinkList } from "@/components/LinkList";
+import { PricingCard } from "@/components/PricingCard";
+import { Section } from "@/components/Section";
+import { TextLink } from "@/components/TextLink";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import { VStack } from "@/components/VStack";
 import { Spacing } from "@/constants/Sizes";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Stack } from "expo-router";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView } from "react-native";
 
 export default function Profile() {
   const colorBackground = useThemeColor({}, "background");
-  const colorText2 = useThemeColor({}, "text2");
 
   return (
     <ScrollView
@@ -25,26 +30,74 @@ export default function Profile() {
           },
         }}
       />
-      <Card>
-        <LinkList
-          data={[
-            {
-              id: "about",
-              label: "About",
-              href: "/profile/about",
-              icon: (
-                <IconSymbol
-                  name="info.square"
-                  size={Spacing.size5}
-                  color={colorText2}
-                />
-              ),
-            },
-          ]}
-        />
-      </Card>
+
+      <VStack style={{ marginBlockEnd: Spacing.navigation * 2 }}>
+        <Section title="Subscriptions">
+          <VStack gap={Spacing.size3}>
+            <PricingCard
+              title="Free Plan"
+              features={["Up to 10 feeds", "Folders"]}
+              pricingMonthly="0€"
+              isActive={true}
+            />
+            <PricingCard
+              title="Basic Plan"
+              features={[
+                "Everything from Free Plan",
+                "Up to 150 feeds",
+                "Newsletter",
+              ]}
+              pricingMonthly="1€"
+              pricingAnnually="0,85€"
+              isActive={false}
+            />
+            <PricingCard
+              title="Pro Plan"
+              pricingMonthly="8€"
+              pricingAnnually="6€"
+              features={["Everything from Basic Plan", "Up to 2500 feeds"]}
+              isActive={false}
+            />
+          </VStack>
+        </Section>
+        <Section title="About">
+          <Card style={{ paddingBlockStart: 0 }}>
+            <LinkList
+              data={[
+                {
+                  id: "website",
+                  href: "https://readii.de",
+                  label: "Website",
+                  icon: <IconSymbol name="globe" />,
+                },
+                {
+                  id: "bluesky",
+                  href: "https://bsky.app/profile/readii.bsky.social",
+                  label: "readii on Bluesky",
+                  icon: <IconSymbol name="person.bubble" />,
+                },
+                {
+                  id: "feedback",
+                  href: "https://wa.me/message/6D6KQFSIQQ6EG1",
+                  label: "Send feedback via WhatsApp",
+                  icon: <IconSymbol name="message" />,
+                },
+              ]}
+            ></LinkList>
+            <ThemedView style={{ marginBlockStart: Spacing.size6 }}>
+              <ThemedText>
+                Moin! This app is built by{" "}
+                <TextLink href="https://bsky.app/profile/lukaskllr.bsky.social">
+                  lukas koeller
+                </TextLink>{" "}
+                & <TextLink href={"https://nordcode.agency"}>nordcode</TextLink>{" "}
+                based in Hamburg. If you ever come by, try a Franzbrötchen. And
+                say &quot;Moin!&quot; to everybody. Tschüss.
+              </ThemedText>
+            </ThemedView>
+          </Card>
+        </Section>
+      </VStack>
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({});
