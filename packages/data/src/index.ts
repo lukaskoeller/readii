8f09411849b4bd43d736c9fdcd55a9c68b688b77
Lukas Koeller <acc.koeller@protonmail.com>
2025-12-18 18:32:28 +0100

    chore(data): improve types, add exports and suggestions

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,14 +1,59 @@
-import { TFeedCategory } from "./suggestions.schema";
+import { TFeedCategory, TFeedSuggestion } from "./suggestions.schema";
+import suggestionsJson from "./suggestions.json";
 
-export const FEED_CATEGORY_TO_LABEL: Record<TFeedCategory, string> = {
-  "news-politics": "News & Politics",
-  entertainment: "Entertainment",
-  sports: "Sports",
-  "startups-innovation": "Startups & Innovation",
+export const FEED_CATEGORY_TO_LABEL = {
+  "home-garden": "Home & Garden",
+  "kids-parenting": "Kids & Parenting",
   "money-business": "Money & Business",
+  "news-politics": "News & Politics",
+  "science-tech": "Science & Tech",
   "software-development": "Software Development",
-  design: "Design",
+  "startups-innovation": "Startups & Innovation",
   "style-beauty": "Style & Beauty",
-  food: "Food",
   "travel-regional": "Travel & Regional",
+  cars: "Cars",
+  design: "Design",
+  entertainment: "Entertainment",
+  food: "Food",
+  health: "Health",
+  hobbies: "Hobbies",
+  outdoors: "Outdoors",
+  sports: "Sports",
+} as const satisfies Record<TFeedCategory, string>;
+
+export type TFeedCategoryLabel = typeof FEED_CATEGORY_TO_LABEL[TFeedCategory];
+
+/**
+ * Iterates over feed suggestions and groups them by category.
+ * @param data Array of feed suggestions
+ * @returns A record mapping each category to its corresponding feed suggestions
+ */
+export const getSuggestionsByCategory = (
+  data: TFeedSuggestion[]
+): Record<TFeedCategory, TFeedSuggestion[]> => {
+  const feedSuggestionsMap = new Map<TFeedCategory, TFeedSuggestion[]>();
+
+  for (const feedSuggestion of data) {
+    for (const category of feedSuggestion.categories) {
+      if (!feedSuggestionsMap.has(category)) {
+        feedSuggestionsMap.set(category, []);
+      }
+      const currentValue = feedSuggestionsMap.get(category) ?? [];
+      feedSuggestionsMap.set(category, [...currentValue, feedSuggestion]);
+    }
+  }
+
+  const result = Object.fromEntries(feedSuggestionsMap) as Record<
+    TFeedCategory,
+    TFeedSuggestion[]
+  >;
+  return result;
 };
+
+export const suggestionsByCategory = getSuggestionsByCategory(
+  suggestionsJson as TFeedSuggestion[]
+);
+
+export const suggestionsByCategoryEntries = Object.entries(
+  suggestionsByCategory
+);
--- packages/data/src/suggestions.schema.ts
+++ packages/data/src/suggestions.schema.ts
@@ -1,16 +1,23 @@
-import { optional, string, z } from "zod/mini";
+import { nullable, string, z } from "zod/mini";
 import { $HttpsUrl } from "@readii/schemas/zod";
 
 export const feedCategory = z.enum([
-  "news-politics",
+  "cars",
+  "design",
   "entertainment",
+  "food",
+  "health",
+  "hobbies",
+  "home-garden",
+  "kids-parenting",
+  "money-business",
+  "news-politics",
+  "outdoors",
+  "science-tech",
+  "software-development",
   "sports",
   "startups-innovation",
-  "money-business",
-  "software-development",
-  "design",
   "style-beauty",
-  "food",
   "travel-regional",
 ]);
 
@@ -21,8 +28,10 @@ export const suggestionSchema = z.object({
   id: $HttpsUrl,
   url: $HttpsUrl,
   feedUrl: $HttpsUrl,
-  description: optional(string()),
+  description: nullable(string()),
   iconUrl: $HttpsUrl,
   ogImageUrl: $HttpsUrl,
   categories: z.array(feedCategory),
 });
+
+export type TFeedSuggestion = z.infer<typeof suggestionSchema>;
