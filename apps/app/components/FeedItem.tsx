88678c05dccbf8a86bf97d40861f9bc435cafa7b
Lukas Koeller <acc.koeller@protonmail.com>
2026-01-23 22:40:09 +0100

    feat(app): add FeedItem component

---
+++ apps/app/components/FeedItem.tsx
@@ -0,0 +1,182 @@
+import { Link } from "expo-router";
+import { ThemedView } from "./ThemedView";
+import { FC } from "react";
+import { Image } from "expo-image";
+import {
+  FontSize,
+  FontWeight,
+  LineHeight,
+  Radius,
+  Spacing,
+} from "@/constants/Sizes";
+import {
+  DateArg,
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInYears,
+} from "date-fns";
+import { dayMonthYearFormat } from "@/core/utils";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { TMediaItem } from "@/core/schema";
+import { IconSymbol } from "./ui/IconSymbol";
+import { StyleSheet, View } from "react-native";
+import { ThemedText } from "./ThemedText";
+
+function formatShortRelative(date: DateArg<Date>) {
+  const now = new Date();
+  const years = differenceInYears(now, date);
+  if (years > 0) return dayMonthYearFormat.format(new Date(date));
+  const days = differenceInDays(now, date);
+  if (days > 0) return `${days}d`;
+  const hours = differenceInHours(now, date);
+  if (hours > 0) return `${hours}h`;
+  const minutes = differenceInMinutes(now, date);
+  if (minutes > 0) return `${minutes}m`;
+  return "now";
+}
+
+export type FeedItemProps = {
+  id: NonNullable<TMediaItem["id"]>;
+  title: TMediaItem["title"];
+  isRead: TMediaItem["isRead"];
+  publishedAt: TMediaItem["publishedAt"];
+  thumbnailUrl: TMediaItem["thumbnailUrl"];
+  previewText: string;
+};
+
+export const FeedItem: FC<FeedItemProps> = ({
+  id,
+  title,
+  isRead,
+  publishedAt,
+  thumbnailUrl,
+  previewText,
+}) => {
+  const colorBackground3 = useThemeColor({}, "background3");
+  const colorText2 = useThemeColor({}, "text2");
+  const colorPrimary = useThemeColor({}, "primary");
+
+  return (
+    <Link
+      href={{
+        pathname: "/home/[mediaItemId]",
+        params: { mediaItemId: id },
+      }}
+    >
+      <ThemedView style={[styles.item]}>
+        <ThemedView>
+          {thumbnailUrl ? (
+            <Image
+              style={styles.thumbnail}
+              source={thumbnailUrl}
+              contentFit="cover"
+              transition={500}
+            />
+          ) : (
+            <ThemedView
+              style={[
+                styles.thumbnail,
+                {
+                  backgroundColor: colorBackground3,
+                },
+              ]}
+            >
+              <IconSymbol
+                size={Spacing.size5}
+                name="photo"
+                color={colorText2}
+              />
+            </ThemedView>
+          )}
+        </ThemedView>
+        <View style={styles.desc}>
+          <ThemedView style={styles.titleBar}>
+            <ThemedText color="text" style={styles.title}>
+              {title}
+            </ThemedText>
+            <ThemedView style={styles.meta}>
+              {!isRead && (
+                <ThemedView
+                  style={[
+                    styles.isUnreadDot,
+                    { backgroundColor: colorPrimary },
+                  ]}
+                />
+              )}
+              <ThemedText type="small">
+                {formatShortRelative(publishedAt)}
+              </ThemedText>
+            </ThemedView>
+          </ThemedView>
+
+          <ThemedText
+            numberOfLines={3}
+            style={[styles.previewText, { color: colorText2 }]}
+          >
+            {previewText}
+          </ThemedText>
+        </View>
+      </ThemedView>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    gap: Spacing.size4,
+    paddingBlock: Spacing.size4,
+  },
+  desc: {
+    flex: 1,
+  },
+  titleBar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "baseline",
+    gap: Spacing.size2,
+  },
+  title: {
+    flex: 1,
+    fontWeight: FontWeight.bold,
+    fontSize: FontSize.size3,
+  },
+  text: {
+    lineHeight: FontSize.size3 * 1.3,
+  },
+  header: {
+    marginBlockStart: Spacing.size4,
+    fontSize: FontSize.size1,
+    textAlign: "right",
+  },
+  thumbnail: {
+    width: Spacing.size9,
+    height: Spacing.size9,
+    borderRadius: Radius.size3,
+    marginBlockStart: Spacing.size1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  meta: {
+    marginBlockStart: Spacing.size1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: Spacing.size1,
+  },
+  isUnreadDot: {
+    width: Spacing.size2,
+    height: Spacing.size2,
+    borderRadius: Radius.full,
+  },
+  previewText: {
+    fontWeight: FontWeight.normal,
+    fontSize: FontSize.size1,
+    lineHeight: LineHeight.size1,
+  },
+});
