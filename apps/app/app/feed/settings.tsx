a1113bed6a41d1eaf96be1abdb133cb23406af8e
Lukas Koeller <acc.koeller@protonmail.com>
2026-02-19 22:41:11 +0100

    chore: rm unused code

--- apps/app/app/feed/settings.tsx
+++ apps/app/app/feed/settings.tsx
@@ -60,7 +60,6 @@ export default function FeedSettings() {
               },
             ]}
           />
-          <ThemedText>{viewMode}</ThemedText>
         </Card>
         <Card
           style={{
